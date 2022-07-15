1d072b26bbc788078aec584e4d5001636e948013
Metrowest <90020717+Metrowest@users.noreply.github.com>
2022-07-15 09:53:45 -0400

    Add files via upload

--- js/interes.js
+++ js/interes.js
@@ -7,36 +7,28 @@
   //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
   let telefono = "14074938534";
   
-  let Fecha = document.querySelector("#Fecha").value;
   let Nombre = document.querySelector("#Nombre").value;
-
   let Sexo = document.querySelector("#Sexo").value;
-  let Direccion1 = document.querySelector("#Direccion1").value;
-  let Ciudad = document.querySelector("#Ciudad").value; 
+  let Direccion = document.querySelector("#Direccion").value;
   let Telefono = document.querySelector("#Telefono").value; 
   let Idioma = document.querySelector("#Idioma").value; 
   let Correo = document.querySelector("#Correo").value;
   let Mejor = document.querySelector("#Mejor").value; 
-  let Entregado = document.querySelector("#Entregado").value;  
-  let Observaciones1 = document.querySelector("#Observaciones1").value; 
+  let Publicador = document.querySelector("#Publicador").value;  
+  let Observaciones = document.querySelector("#Observaciones").value; 
   let resp = document.querySelector("#respuesta");
   resp.classList.remove("fail");
   resp.classList.remove("send");
 
-  let url = `https://api.whatsapp.com/send?phone=14074938534&text=
-  PERSONA QUE MOSTRÓ%0A
+  let url = `https://api.whatsapp.com/send?phone=14074938534&text=PERSONA QUE MOSTRÓ INTERÉS%0A
 
-  Fecha%0A
-  ${Fecha}%0A
+
   Nombre%0A
   ${Nombre}%0A
-
   Sexo%0A
   ${Sexo}%0A
-  Direccion1%0A
-  ${Direccion1}%0A
-  Ciudad%0A
-  ${Ciudad}%0A
+  Direccion%0A
+  ${Direccion}%0A
   Telefono%0A
   ${Telefono}%0A
   Idioma%0A
@@ -45,12 +37,12 @@
   ${Correo}%0A
   Mejor momento para contactar%0A
   ${Mejor}%0A
-  Entregado%0A
-  ${Entregado}%0A
-  Observaciones1%0A
-  ${Observaciones1}`;
+  Publicador%0A
+  ${Publicador}%0A
+  Observaciones%0A
+  ${Observaciones}`;
 
-  if (Fecha === "" || Nombre === "" ) {
+  if (Nombre === "" || Idioma === "" ) {
     resp.classList.add("fail");
     resp.innerHTML = `Faltan algunos datos, ${Nombre}`;
     return false;
